078b97f5cc328402f1c7ec21d3b9732832e28da6
Meredith Malak <mer87099@gmail.com>
2020-04-28 03:35:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello" + {specialGuest} + "!";
 
-var topic = `Hello, ${specialGuest}! High ${3 + 2}!`; // 
+var topic =  "space";
 
-var conversation = `${topic}`;
+var conversation = "Let\'s talk about" + {topic} + specialGuest;
 
 
 
